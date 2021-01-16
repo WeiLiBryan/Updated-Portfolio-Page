639b7700451127237b0ef1ba3ad8e54804be4264
William Bryan <wwbryan00@gmail.com>
2021-01-16 04:36:03 -0800

    add project section

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.sidebar').on("click", "a", function(){
+        var cl = this.class;
+
+        switch(cl){
+            case "about-btn":
+                $('#about-sec').attr('class', 'show about-me');
+                $('#proj-sec').attr('class', 'hide proj-sec');
+                break;
+            case "proj-btn":
+                $('#about-sec').attr('class', 'show about-me');
+                $('#proj-sec').attr('class', )
+                break;
+        }
+    });
+
+});
